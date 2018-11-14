16068916d3b7cb8a227b14be02516f9fbc365f27
al06208 <al06208@CEIT2551110X001.ad.georgiasouthern.edu>
2018-11-13 19:09:56 -0500

    other deities

--- src/mainPackage/DFAChecker.java
+++ src/mainPackage/DFAChecker.java
@@ -7,8 +7,9 @@ public class DFAChecker {
 	public static void main(String[] args) {
 		StringParser sp = new StringParser(args[0]);
 		DFA brumby = sp.export();
+		System.out.println("Enter a string of length 1 or more:");
 		Scanner grumb = new Scanner(System.in);
-		String input = grumb.next();
+		String input = grumb.nextLine();
 		grumb.close();
 		if(brumby.checkString(input)) System.out.println("String accepted.");
 		else System.out.println("String rejected.");
